fa46a6aa6a908b44f3b13c2757778d0edee8355b
namangulati123 <89136985+namangulati123@users.noreply.github.com>
2022-06-03 13:56:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var first_name = Sara;
+var last_name = Singh;
+
+var a = 10;
+var b = 5;
+
+first_name.concat(last_name);
+
+var add = a+b;
+console.log(add);
+
+var sub = a-b;
+console.log(sub);
+
+var mul = a*b;
+console.log(mul);
+
+var div = a/b;
+console.log(div);
